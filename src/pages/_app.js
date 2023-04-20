20f04a4a5956f6c50d8baa1bc61939df80e56635
Sara <saeraa@gmail.com>
2023-04-20 21:24:30 +0200

    unbreaking some shit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.scss";
-import Navbar from "@/components/navBar/NavBar";
+import Navbar from "@/components/navBar/navBar";
 import Footer from "@/components/footer/footer";
 
 export default function App({ Component, pageProps }) {

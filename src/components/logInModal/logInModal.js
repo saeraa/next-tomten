4103f341be42dad05823a2c83694e4245af846be
David Lidebrandt <david.lidebrandt@gmail.com>
2023-04-30 22:29:54 +0200

    Add functions for setting form

--- src/components/logInModal/logInModal.js
+++ src/components/logInModal/logInModal.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import LogInForm from "../logInForm/LogInForm";
 import styles from "@/styles/logInModal.module.scss";
 
 const LogInModal = ({ showLogInModal, handleShowLogInModal }) => {
+  const [form, setForm] = useState("login");
+  const setToRegister = () => {
+    setForm("register");
+  };
+  const setToForgotPassword = () => {
+    setForm("password");
+  };
+  const setToAnonymous = () => {
+    setForm("anonymous");
+  };
+  const setToLogin = () => {
+    setForm("login");
+  };
+
   if (!showLogInModal) {
     return null;
-  } else {
+  } else if (form === "login") {
     return (
       <div className={styles.logInModal}>
         <LogInForm></LogInForm>

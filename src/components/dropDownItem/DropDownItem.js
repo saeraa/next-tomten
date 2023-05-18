c12bf9efc32adecb95f2e9c30797adcb4c4c37cf
David Lidebrandt <david.lidebrandt@gmail.com>
2023-05-18 19:26:06 +0200

    Add function for hover/click dropwdown menu items

--- src/components/dropDownItem/DropDownItem.js
+++ src/components/dropDownItem/DropDownItem.js
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import Link from "next/link";
 import styles from "@/styles/header.module.scss";
 
-const DropDownItem = ({ text, content }) => {
+const DropDownItem = ({
+  idx,
+  showDropDown,
+  setShowDropDown,
+  text,
+  content
+}) => {
+
   return (
-    <li className={styles.navListItem}>
+    <li
+      onMouseEnter={() => {
+        setShowDropDown(idx);
+      }}
+      onMouseLeave={() => {
+        setShowDropDown(null);
+      }}
+      onClick={() => {
+        setShowDropDown(idx);
+      }}
+      className={styles.navListItem}
+    >
       <div className={styles.dropdown}>
         <button className={styles.dropdownButton}>
           {text}
           <i class="fa fa-caret-down"></i>
         </button>
-        <div className={styles.dropdownContent} id={styles.dropdownRepertoar}>
+        <div className={`${styles.dropdownContent} ${showDropDown === idx ? styles.show : null}`}>
           {content.map((item) => {
             return <Link href={item.href}>{item.text}</Link>;
           })}

fe3aec3ce99d1e4c368de7de6a45310b5a344f13
David Lidebrandt <david.lidebrandt@gmail.com>
2023-05-18 17:49:15 +0200

    Add sub menu

--- src/components/dropDownItem/DropDownItem.js
+++ src/components/dropDownItem/DropDownItem.js
@@ -1,35 +1,17 @@
 import Link from "next/link";
 import styles from "@/styles/header.module.scss";
 
-const DropDownItem = ({
-  idx,
-  showDropDown,
-  setShowDropDown,
-  text,
-  content
-}) => {
-
+const DropDownItem = ({ text, content }) => {
   return (
-    <li
-      onMouseEnter={() => {
-        setShowDropDown(idx);
-      }}
-      onMouseLeave={() => {
-        setShowDropDown(null);
-      }}
-      onClick={() => {
-        setShowDropDown(idx);
-      }}
-      className={styles.navListItem}
-    >
+    <li className={styles.navListItem}>
       <div className={styles.dropdown}>
         <button className={styles.dropdownButton}>
           {text}
-          <i className="fa fa-caret-down"></i>
+          <i class="fa fa-caret-down"></i>
         </button>
-        <div className={`${styles.dropdownContent} ${showDropDown === idx ? styles.show : null}`}>
+        <div className={styles.dropdownContent} id={styles.dropdownRepertoar}>
           {content.map((item) => {
-            return <Link key={Math.random() * 13377331} href={item.href}>{item.text}</Link>;
+            return <Link href={item.href}>{item.text}</Link>;
           })}
         </div>
       </div>

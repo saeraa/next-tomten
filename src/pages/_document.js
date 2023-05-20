4e021e2ad1e550d01486f2e59a1644f82669e52b
Sara <saeraa@gmail.com>
2023-05-20 13:25:07 +0200

    minor fixes; font & favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,11 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" type="image/svg+xml" href="logo.svg" />
+        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
       </Head>
       <body>
         <Main />
-
         <NextScript />
       </body>
     </Html>
